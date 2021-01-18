d6e7175057f48ac5f0940877bb454feaa09f4cdb
Bambi <yuqifang.school@gmail.com>
2021-01-17 19:00:44 -0800

    test hook

---
+++ src/Checkbox.test.js
@@ -0,0 +1,10 @@
+import {render, fireEvent} from "@testing-library/react";
+import {Checkbox} from "./Checkbox";
+import React from "react";
+
+test("Selecting checkbox", () => {
+    const {getByLabelText} = render(<Checkbox />);
+    const checkbox = getByLabelText(/unchecked/);
+    fireEvent.click(checkbox);
+    expect(checkbox.checked).toEqual(true);
+});
